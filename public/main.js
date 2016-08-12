f6226a601b44691b58563c9c2d3f6b41fbf2c4f8
marcelo lopes de sousa lima <marcelo.lopesousa@gmail.com>
2016-08-12 18:36:52 -0300

    erro -01

--- public/main.js
+++ public/main.js
@@ -32,7 +32,7 @@ myApp.controller('mainCtrl', function($scope, $timeout, $http){
       
       			$scope.retorno = r.data;
       			$timeout(poller, 1000);
-    		}).error(function(){
+    		}, function(){
     			document.getElementById('alert-field').appendChild('<div class="alert alert-warning alert-dismissible" role="warn"> Erro ao acessar: http://172.30.121.230:4568/api/list </div>');	
     		});
     
@@ -46,7 +46,7 @@ myApp.controller('mainCtrl', function($scope, $timeout, $http){
 			series2.append(new Date().getTime(), $scope.status.enqueued);
 			series.append(new Date().getTime(), $scope.status.processed);
                         $timeout(poller2, 5000);
-                }).error(function(){
+                }, function(){
     			document.getElementById('alert-field').appendChild('<div class="alert alert-warning alert-dismissible" role="warn"> Erro ao acessar: http://172.30.121.230:4568/api/status </div>"');	
     		});
 
